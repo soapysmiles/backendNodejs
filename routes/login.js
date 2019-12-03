ab9329cb40a9d9468f9e2fa18a49d90bffe64159
machina2 <machina2@coventry.ac.uk>
2019-12-03 21:00:38 +0000

    Edit to login.js Added error catching

--- routes/login.js
+++ routes/login.js
@@ -9,23 +9,30 @@ var router = Router({
 
 var bodyParser = require('koa-bodyparser');
 
-router.post(`/addUser`,bodyParser(), async(ctx, next) => {
-    const body = ctx.request.body;
-    const user = {
-        username : body.username,
-        password :body.password,
-        fName : body.fName,
-        lName : body.lName,
-        email :body.email,
-        about :body.about || '',
-        countryID : body.countryID,
-        birthDate : body.birthDate
-    }
-    console.log(user)
+router.post(`/register`,bodyParser(), async(ctx, next) => {
+    try{
+        const body = ctx.request.body;
+        const user = {
+            username : body.username,
+            password :body.password,
+            fName : body.fName,
+            lName : body.lName,
+            email :body.email,
+            about :body.about || '',
+            countryID : body.countryID,
+            birthDate : body.birthDate
+        }
 
-    let item = await loginModel.newUser(user);
-    ctx.body = item;
-    //TODO: add some image upload
+        let item = await loginModel.register(ctx, user);
+        ctx.body = item;
+        //TODO: add some image upload
+        ctx.response.status = 201;
+        ctx.body = {message:"added successfully"};
+    }catch(error){
+        ctx.response.status = error.status;
+        ctx.body = {message:error.message};
+    }
 });
+
 
 module.exports = router;
