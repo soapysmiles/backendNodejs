7c65bc32e569fa6d63d657d3a09e94657183db3f
machina2 <machina2@coventry.ac.uk>
2019-12-03 19:43:53 +0000

    user adding

--- models/login.js
+++ models/login.js
@@ -2,17 +2,28 @@ var mysql = require('promise-mysql');
 var bcrypt = require('bcrypt');
 var info = require('../config');
 
-
+var Pass = require('../modules/password')
 const Valid = require('../modules/validator')
-const valid = new Valid()
+
+
+
+function convertDate(dateString){
+    var parts = dateString.split('-');
+    var result = new Date(parts[0], parts[1] - 1, parts[2]); 
+    return result;
+}
+
 
 exports.newUser = async(data) => {
+    const valid = new Valid()
+    const pass = new Pass()
     try{
         const connection = await mysql.createConnection(info.config);
         const words = [data.username,data.fName,data.lName,data.about]
         for(let i = 0; i < words.length; i++) valid.checkWord(words[i], i)//Check words are valid
-        
-
+        const passHash = pass.encrypt(data.password, null);
+        const birthDate = convertDate(data.birthDate).toISOString().slice(0, 19).replace('T', ' ')
+        const regDateTime = new Date().toISOString().slice(0, 19).replace('T', ' ')
         const sql = `INSERT INTO user(
             username,
             password,
@@ -27,8 +38,20 @@ exports.newUser = async(data) => {
             active,
             deleted) VALUES (
                 "${data.username}",
-                
+                "${passHash.hash}",
+                "${passHash.salt}",
+                "${data.fName}",
+                "${data.lName}",
+                "${data.email}",
+                "${data.about}",
+                "${data.countryID}",
+                "${birthDate}",
+                "${regDateTime}",
+                1,
+                0
             )`
+
+            await connection.query(sql);
 
         connection.end()
         return {message:"created successfully"};
--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ var Router = require('koa-router');
 var loginModel = require('../models/login');
 var bodyParser = require('koa-bodyparser');
 
+
 var router = Router({
     prefix: '/login'
 });
@@ -12,13 +13,13 @@ router.post(`/addUser`,bodyParser(), async(ctx, next) => {
     const body = ctx.request.body;
     const user = {
         username : body.username,
-        pass :body.password,
+        password :body.password,
         fName : body.fName,
         lName : body.lName,
         email :body.email,
         about :body.about || '',
         countryID : body.countryID,
-        birthDate : body.birthdate
+        birthDate : body.birthDate
     }
     console.log(user)
 
