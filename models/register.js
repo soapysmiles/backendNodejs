ca750600bf8fce4b4a551d586eaea59cfc6bc218
machina2 <machina2@coventry.ac.uk>
2019-12-03 23:59:29 +0000

    Add validation

--- models/register.js
+++ models/register.js
@@ -8,7 +8,6 @@ var user = require('../modules/user')
 
 
 exports.register = async(ctx, data) => {
-    const valid = new Valid()
     const pass = new Pass()
     try{
         //Check if user exists
@@ -20,14 +19,33 @@ exports.register = async(ctx, data) => {
         const connection = await mysql.createConnection(info.config);
 
         //Check words match requirements
-        const words = [data.username,data.fName,data.lName,data.about]
-        for(let i = 0; i < words.length; i++) valid.checkWord(words[i], i)
+        const words = [
+            {data: data.username, name: 'username'},
+            {data: data.fName, name: 'first name'},
+            {data: data.lName, name: 'last name'},
+            {data: data.about, name: 'about'}
+        ]
+        for(let i = 0; i < words.length; i++) {
+            try{
+                Valid.checkWord(words[i].data, words[i].name)
+            }catch(err){
+                throw {message: err.message, status:400};
+            }
+        }
+
+        //Check email
+        try{
+            Valid.checkEmail(data.email)
+        }catch(err){
+            throw {message: err.message, status:400};
+        }
+        
 
         //Encrypt password
         const passHash = pass.encrypt(data.password, null);
 
         //Convert text date to mySQL date
-        const birthDate = convertDate(data.birthDate).toISOString().slice(0, 19).replace('T', ' ')
+        const birthDate = Valid.convertDate(data.birthDate).toISOString().slice(0, 19).replace('T', ' ')
 
         //Get current date, convert to mySQL format
         const regDateTime = new Date().toISOString().slice(0, 19).replace('T', ' ')
@@ -62,10 +80,9 @@ exports.register = async(ctx, data) => {
         await connection.query(sql);
 
         connection.end()
-        ctx.response.stats = 201;
         return {message:"created successfully"};
     }catch (error) {
-        if(error.status === undefined)
+        if(error.status === undefined || isNaN(error.status))
             error.status = 500;
         throw error;
     }
