2895b80d131c9b7a05716cd6ebe59e337a3726b7
machina2 <machina2@coventry.ac.uk>
2019-12-04 10:33:41 +0000

    Edit - changed to exports

--- modules/password.js
+++ modules/password.js
@@ -1,20 +1,14 @@
 var bcrypt = require('bcrypt');
 
-module.exports = class password {
-    constructor(){
-        return this
-    }
-
-    encrypt(password, preSalt) {
-        const salt = preSalt || bcrypt.genSaltSync(10)
-        const hash = bcrypt.hashSync(password, salt)
-        return {hash: hash, salt: salt}   
-    }
+exports.encrypt = (password, preSalt) => {
+    const salt = preSalt || bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+    return {hash: hash, salt: salt}   
+}
     
 
-    comparePassword(plainPass, salt, hashword) {
-        const passHash = bcrypt.hashSync(plainPass, salt)
-        if(hashword === passHash) return true
-        return false
-    }
+exports.comparePassword = (plainPass, salt, hashword) => {
+    const passHash = bcrypt.hashSync(plainPass, salt)
+    if(hashword === passHash) return true
+    return false
 }
