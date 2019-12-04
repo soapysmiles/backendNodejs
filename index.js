dde8281d1220b6beff509ced9f995fd687198322
machina2 <machina2@coventry.ac.uk>
2019-12-04 14:49:13 +0000

    add login to index

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var Koa = require('koa');
-
 var admin = require('./routes/admin');
+var login = require('./routes/login')
 var register = require('./routes/register');
 
 var app = new Koa();
 
-
+app.use(login.routes());
 app.use(admin.routes());
-app.use(register.routes())
+app.use(register.routes());
+
 var port = process.env.PORT || 3000;
 
 app.listen(port);
