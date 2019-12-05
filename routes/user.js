b02f2b8f1c318a9eeb5e355bb22162f0307044d3
machina2 <machina2@coventry.ac.uk>
2019-12-05 15:40:11 +0000

    Clean user.js

--- routes/user.js
+++ routes/user.js
@@ -1,39 +1,31 @@
 var Router = require('koa-router');
 var userModel = require('../models/userDoa');
 var bodyParser = require('koa-bodyparser');
-
+const passport = require('koa-passport');
 
 const koaBody = require('koa-body')({multipart: true, uploadDir: '.'})
 var router = Router({
     prefix: '/api/v1.0.0'
 });
 
-const passport = require('koa-passport');
-require("../auth/auth");
-passport.initialize()
 
 
-router.get(`/user/:ID([0-9]{1,})`, async(ctx, next) => {
-    //Authenticate user entry
-    return passport.authenticate("jwt", { session: false }, async (err, payload) =>{
-        try{
+router.get(`/user/:ID([0-9]{1,})`,passport.authenticate("jwt", { session: false }), async(ctx, next) => { 
+    try{
+        const ID = ctx.params.ID;
+        
+        //Gets user information
+        const user = await userModel.getOneByID(ID).catch((err) => {
             if(err) throw {message: err.message}
-            const ID = ctx.params.ID;
-            //if(payload.ID !== ID) throw {message: 'Unauthorised'} //Checks if user is accessing own page
-            
-            //Gets user information
-            const user = await userModel.getOneByID(ID).catch((err) => {
-                if(err) throw {message: err.message}
-            });
+        });
 
-            //Sets return to user data
-            ctx.body = user;
-            ctx.response.status = 200;//a-o-kay
-        }catch(error){
-            ctx.response.status = error.status || 400;
-            ctx.body = {message:error.message};
-        }
-    })(ctx)
+        //Sets return to user data
+        ctx.body = user;
+        ctx.response.status = 200;//a-o-kay
+    }catch(error){
+        ctx.response.status = error.status || 400;
+        ctx.body = {message:error.message};
+    }
 });
 
 module.exports = router;
