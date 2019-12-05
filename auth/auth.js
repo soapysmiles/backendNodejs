89101bb09add69bb438b09b4991f3351196faf8e
machina2 <machina2@coventry.ac.uk>
2019-12-05 14:50:29 +0000

    Edit error return auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -6,13 +6,16 @@ var opts = {};
 var jwt = require("jwt-simple");
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = cfg.jwt.jwtSecret;
-var User = require('../modules/user')
+var User = require('../models/user')
 passport.use(
   new JwtStrategy(opts, async (jwt_payload, done) => {
-    const user = await User.getOneByID(jwt_payload.ID).catch((e) => {
-        done(null, null)
+    const user = await User.getJWT(jwt_payload.ID).catch((e) => {
+        done({message: 'Unauthorised'}, null)
     })
-    if(jwt.encode(jwt_payload, cfg.jwt.jwtSecret) !== user.jwt) done(null,null)
+    if(!user)done({message: 'Unauthorised'}, null)
+    if(jwt.encode(jwt_payload, cfg.jwt.jwtSecret) !== user.jwt) done({message: 'Unauthorised'},null)
     done(null, jwt_payload);
   })
 );
+
+
