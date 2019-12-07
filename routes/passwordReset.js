b138112ac0531217e7bc81301220bd979b5f7edf
machina2 <machina2@coventry.ac.uk>
2019-12-07 21:17:54 +0000

    feat: [password reset] add routes: post reset password post answers get questions put questions

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -7,21 +7,120 @@ var router = Router({
     prefix: '/api/v1.0.0/passwordreset'
 });
 
-router.get(`/:ID([0-9]{1,})`, async(ctx, next) => { 
+
+/**
+ * @name put/QuestionsAnswers
+ * @inner
+ * @param {int} ID of user
+ * @param {string} question1
+ * @param {string} question2
+ * @param {string} answer1
+ * @param {string} answer2
+ */
+router.put(`/`, koaBody, async(ctx, next) => { 
     return passport.authenticate("jwt", { session: false }, async (err, payload) => {//Get payload
         try{
-            const ID = ctx.params.ID;
+            const body = ctx.request.body;
+            const ID = body.userID;
             if(payload.ID != ID) throw {message: 'Unauthorised', status: 401} //Checks if user is accessing own page
+            const data = {
+                question1: body.question1,
+                question2: body.question2,
+                answer1: body.answer1,
+                answer2: body.answer2
+            }
             //Gets user questions
-            passwordResetModel.getQuestionsAnswers(ID)
+            await passwordResetModel.setQuestionsAnswers(ID, data)
             //Sets return to user data
-            ctx.body = questions;
+            ctx.body = {message: 'Questions and answers set'};
             ctx.response.status = 200;//a-o-kay
         }catch(error){
             ctx.response.status = error.status || 400;
             ctx.body = {message:error.message};
         }
     })(ctx)
+});
+
+/**
+ * @name get/QuestionsAnswers
+ * @inner
+ * @param {int} ID userID to get questions and answers
+ */
+router.get(`/:ID([0-9]{1,})`, async(ctx, next) => { 
+    try{
+        const ID = ctx.params.ID;
+        //Gets user questions
+        const questions = await passwordResetModel.getQuestions(ID)
+        //Sets return to user data
+        ctx.body = questions;
+        ctx.response.status = 200;//a-o-kay
+    }catch(error){
+        ctx.response.status = error.status || 400;
+        ctx.body = {message:error.message};
+    }
+});
+
+/**
+ * @name post/Answers
+ * @inner
+ * @param {int} ID userID to get questions and answers
+ * @param {string} answer1
+ * @param {string} answer2
+ * @returns {Object} code for reset consisting of hash and salt
+ */
+router.post(`/`, koaBody, async(ctx, next) => { 
+    try{
+        const body = ctx.request.body;
+        const ID = body.userID;
+        
+        const data = {
+            answer1: body.answer1,
+            answer2: body.answer2
+        }
+        //Check user answers
+        const result = await passwordResetModel.checkAnswers(ID, data)
+            .catch((e) => {throw e})
+            .then((res) => {
+                return res
+            })
+        //Sets return to user data
+        ctx.body = {code: result};
+        ctx.response.status = 200;//a-o-kay
+    }catch(error){
+        ctx.response.status = error.status || 400;
+        ctx.body = {message:error.message};
+    }
+});
+
+
+/**
+ * @name post/Reset New password
+ * @inner
+ * @param {int} ID userID to get questions and answers
+ * @param {string} answer1
+ * @param {string} answer2
+ * @param {string} newPassword
+ */
+router.post(`/reset`, koaBody, async(ctx, next) => { 
+    try{
+        const body = ctx.request.body;
+        const ID = body.userID;
+        
+        const data = {
+            codeHash: body.codeHash,
+            codeSalt: body.codeSalt,
+            newPassword: body.newPassword
+        }
+        //Change user password
+        await passwordResetModel.passwordReset(ID, data)
+            .catch((e) => {throw e})
+        
+        ctx.body = {message:"Password reset"};
+        ctx.response.status = 200;//a-o-kay
+    }catch(error){
+        ctx.response.status = error.status || 400;
+        ctx.body = {message:error.message};
+    }
 });
 
 module.exports = router;
